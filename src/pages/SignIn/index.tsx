d1ab32046f02c0cdce902b0b0a8c94a3bc165eec
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-13 09:53:48 -0300

    add: async storage

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -16,6 +16,8 @@ import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
+import { useAuth } from '../../hooks/auth';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -42,6 +44,10 @@ const SignIn: React.FC = () => {
   const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
 
+  const { signIn, user } = useAuth();
+
+  console.log(user);
+
   const handleSignIn = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
@@ -57,12 +63,10 @@ const SignIn: React.FC = () => {
         abortEarly: false,
       });
 
-      /*await signIn({
-          email: data.email,
-          password: data.password,
-        });*/
-
-      //history.push('/dashboard');
+      await signIn({
+        email: data.email,
+        password: data.password,
+      });
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
